d70b8dabcb58e23489b156c01a1197eef786d19f
Anton Wilhelm <timaschew@gmail.com>
2014-10-13 00:42:47 +0200

    provide loader and latest

--- index.js
+++ index.js
@@ -20,6 +20,27 @@ function require(name) {
 }
 
 /**
+ * Meta info, accessible in the global scope unless you use AMD option.
+ */
+
+require.loader = 'component';
+
+/**
+ * Find and require a module which name starts with the provided name.
+ * If multiple modules exists, the highest semver is used. 
+ * This function should be used for remote dependencies.
+ */
+
+require.latest = function (name) {
+  var available = Object.keys(require.modules).filter(function(moduleName) {
+    return moduleName.indexOf(name) !== -1
+  });
+  if (available.length === 0) {
+    throw new Error('failed to find latest module of "' + name + '"');
+  }
+  return require(available.sort().pop());
+}
+/**
  * Registered modules.
  */
 
